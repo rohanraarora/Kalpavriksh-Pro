dd37a4b6cbe62f861e2dd091f9c263e55e515a86
Sahib Sethi <sahib.sethi30@gmail.com>
2015-08-31 01:45:29 +0530

    Updated Databases with superTest-Not completed.

--- app/src/main/java/in/kvsc/kalpavrikshpro/HomeActivity.java
+++ app/src/main/java/in/kvsc/kalpavrikshpro/HomeActivity.java
@@ -1,26 +1,45 @@
 package in.kvsc.kalpavrikshpro;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.squareup.okhttp.MultipartBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+
 import utilities.Constant;
 import utilities.TabsAdapter;
+import utilities.Utilities;
 
 public class HomeActivity extends AppCompatActivity {
 
 
     TabLayout mTabLayout;
     ViewPager mPager;
+    View parentView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
+        parentView = this.findViewById(R.id.home_screen);
 
         //View Pager
         mPager = (ViewPager) this.findViewById(R.id.viewpager);
@@ -32,8 +51,13 @@ public class HomeActivity extends AppCompatActivity {
         //Tab Layout
         mTabLayout = (TabLayout)this.findViewById(R.id.tab_layout);
         mTabLayout.setupWithViewPager(mPager);
+
+        updateDatabase();
     }
 
+    private void updateDatabase(){
+
+    }
     private void setupViewPager() {
 
         //Set Up View pager
@@ -78,7 +102,69 @@ public class HomeActivity extends AppCompatActivity {
             this.finish();
             return true;
         }
+        else if(id == R.id.action_update_database){
+            if(Utilities.isConnectionAvailable(this)){
+                new UpdateDatabseAsyncTask().execute();
+            }
+            else{
+                Snackbar.make(parentView, "Please connect to Internet", Snackbar.LENGTH_LONG).show();
+            }
+        }
+
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public class UpdateDatabseAsyncTask extends AsyncTask<String, Void, JSONObject> {
+        ProgressDialog mProgressDialog;
+        String token;
+        @Override
+        protected JSONObject doInBackground(String... params) {
+
+            SharedPreferences user_pref = getSharedPreferences(Constant.USER_SHARED_PREFS,MODE_PRIVATE);
+            token = user_pref.getString(Constant.USER_TOKEN,"");
+            String superTestResponse;
+            JSONObject superTestResponseJsonObject = new JSONObject();
+            try {
+                superTestResponse = post(Constant.SUPERTEST_URL);
+                Log.i("string", superTestResponse);
+                superTestResponseJsonObject = new JSONObject(superTestResponse);
+                Log.i("Object" , superTestResponseJsonObject.toString());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return superTestResponseJsonObject;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            mProgressDialog = ProgressDialog.show(HomeActivity.this, null, "Loading...");
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject jsonObject) {
+            super.onPostExecute(jsonObject);
+            mProgressDialog.dismiss();
+        }
+        String post(String url) throws IOException {
+            //Using OkHttp lib
+            OkHttpClient client = new OkHttpClient();//creating client
+
+            Request request = new Request.Builder()//building request
+                    .url(url)
+                    .header("Authorization",token)
+                    .build();
+            try {
+                Response response = client.newCall(request).execute();
+                String responseString = response.body().string();
+                response.body().close();
+                return responseString;
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+                Snackbar.make(parentView,"Cannot connect to server. Please try again later",Snackbar.LENGTH_SHORT).show();
+                return null;
+            }
+        }
     }
 }
